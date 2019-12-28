2c16a3542dafe7e94e0f1a337648dfcbf8f74fa5
Danny <mezenov.201521@gmail.com>
2019-12-28 15:18:10 +0300

    Добавлены разные анимации при переходе  разными способами

--- app/src/main/java/com/danny/swipesmanager/BottomActivity.java
+++ app/src/main/java/com/danny/swipesmanager/BottomActivity.java
@@ -2,6 +2,7 @@ package com.danny.swipesmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.transition.Transition;
 import android.transition.TransitionInflater;
@@ -12,7 +13,16 @@ public class BottomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom);
-        Transition transition = TransitionInflater.from(this).inflateTransition(R.transition.slide_down);
-        getWindow().setEnterTransition(transition);
+        Intent intent = getIntent();
+        if(intent.getStringExtra("animation").equals("slide_up")){
+            Transition transition = TransitionInflater.from(this).inflateTransition(R.transition.slide_down);
+            getWindow().setEnterTransition(transition);
+        }
+        else if(intent.getStringExtra("animation").equals("on_click")){
+            Transition transition = TransitionInflater.from(this).inflateTransition(R.transition.fade);
+            getWindow().setEnterTransition(transition);
+        }
+
+
     }
 }
--- app/src/main/java/com/danny/swipesmanager/MainActivity.java
+++ app/src/main/java/com/danny/swipesmanager/MainActivity.java
@@ -9,14 +9,18 @@ import android.transition.Transition;
 import android.transition.TransitionInflater;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    GestureDetector gestureDetector;
+    private GestureDetector gestureDetector;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btn);
         getWindow().setAllowEnterTransitionOverlap(false);
         gestureDetector = new GestureDetector(this, new OnSwipeListener() {
             @Override
@@ -42,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
                 Transition transition_up = TransitionInflater.from(MainActivity.this).inflateTransition(R.transition.slide_up);
                 getWindow().setExitTransition(transition_up);
                 Intent intent = new Intent(MainActivity.this, BottomActivity.class);
+                intent.putExtra("animation", "slide_up");
                 ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this);
                 startActivity(intent, options.toBundle());
             }
@@ -51,6 +56,17 @@ public class MainActivity extends AppCompatActivity {
                 Transition transition_down = TransitionInflater.from(MainActivity.this).inflateTransition(R.transition.slide_down);
                 getWindow().setExitTransition(transition_down);
                 Intent intent = new Intent(MainActivity.this, TopActivity.class);
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this);
+                startActivity(intent, options.toBundle());
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Transition transition_up = TransitionInflater.from(MainActivity.this).inflateTransition(R.transition.fade);
+                getWindow().setExitTransition(transition_up);
+                Intent intent = new Intent(MainActivity.this, BottomActivity.class);
+                intent.putExtra("animation", "on_click");
                 ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this);
                 startActivity(intent, options.toBundle());
             }
